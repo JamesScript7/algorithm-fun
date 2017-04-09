258371b6277173b98ad1199021858f71c20418f4
James Kim <james.kim7@yahoo.com>
2017-04-09 18:27:37 -0400

    Added Initial data for the roman numeral

---
+++ roman-numeral-converter.js
@@ -0,0 +1,15 @@
+function convertToRoman(num) {
+  let numArr = [];
+  numArr.push(num);
+
+  console.log(numArr);
+  numArr.split("");
+
+  // for (let i = 0; i < num.length; i++) {
+  //   console.log(num.indexOf(i));
+  //   console.log(numArr);
+  // }
+  return numArr;
+}
+
+convertToRoman(36);
