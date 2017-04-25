7373e666d4c014c243fd9d53a831b3455ca6c91b
James Kim <james.kim7@yahoo.com>
2017-04-25 00:47:28 -0400

    Worked on quick-sort
    Need to work on adding the function inside of the function

--- quick-sort.js
+++ quick-sort.js
@@ -1,3 +1,46 @@
 (function(){
-  
+  var QuickSort = function(inputArr) {
+    var
+      len = inputArr.length,
+      left = [],
+      right = [],
+      pivot;
+
+    if (len === 0) {
+      return inputArr;
+    }
+
+    pivot = inputArr[0];
+
+    for (var i = 1; i < len; i++) {
+      if (inputArr[i] <= pivot) {
+        left.push(inputArr[i]);
+      } else {
+        right.push(inputArr[i]);
+      }
+    }
+
+
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
