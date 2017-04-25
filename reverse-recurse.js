707a38c1d87909f4e205a4ca6ae1f520b513d8f1
James Kim <james.kim7@yahoo.com>
2017-04-24 21:33:47 -0400

    Cleaned up reverse-recurse
    Added quick-sort algo practice

--- reverse-recurse.js
+++ reverse-recurse.js
@@ -20,22 +20,3 @@ function recurse(x) {
 }
 
 recurse(x);
-
-// Example Two
-
-var str = "doggo";
-
-//using recursive:
-
-var result = [];
-
-function recurse(x) {
-  if (x.length <= 0) {
-    return;
-  } else {
-    result.push(str[x - 1]);
-    return x - ( recurse(x - 1) );
-  }
-}
-
-recurse(str);
