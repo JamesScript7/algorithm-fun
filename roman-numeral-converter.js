51f9cd456c5791b03b58f0da6f3359eda417651a
James Kim <james.kim7@yahoo.com>
2017-04-28 19:24:09 -0400

    Working on Roman Numeral Converter
    - Added symbols

--- roman-numeral-converter.js
+++ roman-numeral-converter.js
@@ -3,33 +3,47 @@
 function convertToRoman(num) {
   let
     arr = num.toString().split("").reverse(),
+    romanArr = [],
     romanNum = {
       one: "I",
       five: "V",
       ten: "X",
-      fifty: "L"
+      fifty: "L",
+      hundred: "C",
+      fiveHundred: "D",
+      thousand: "M"
     },
     res = "";
 
+  // I, II, III, "IV", V, VI, VII, VIII, IX, and X.
+  // Add function that handles 4 and 9.
+  // if multiple of 5?
+
+  // Ones place
   if (arr[0]) {
     for (let j = 0; j < arr[0]; j++) {
     res += romanNum.one;
     }
 
-    arr[0] = res;
+    romanArr[0] = res;
     res = "";
   }
-
+  // Tenths place
   if (arr[1]) {
     for (let k = 0; k < arr[1]; k++) {
       res += romanNum.ten;
     }
 
-    arr[1] = res;
+    romanArr[1] = res;
     res = "";
   }
 
+  // Hundreths place
+
+  // Thousandsths place
+
   console.log(arr);
+  console.log(romanArr);
   // return res;
 }
 
