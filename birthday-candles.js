f1eef71c0c9bb599d08a6e443c82ba5d8a40f84e
James Kim <james.kim7@yahoo.com>
2017-05-07 17:29:35 -0400

    Added birthday-candles

---
+++ birthday-candles.js
@@ -0,0 +1,22 @@
+// Colleen is turning  years old! She has
+// candles of various heights on her cake,
+// and candle  has height . Because the taller
+// candles tower over the shorter ones, Colleen
+// can only blow out the tallest candles.
+//
+// Given the  for each individual candle,
+// find and print the number of candles she
+// can successfully blow out.
+//
+// Input Format
+//
+// The first line contains a single integer, ,
+// denoting the number of candles on the cake.
+// The second line contains  space-separated
+// integers, where each integer  describes the height of candle .
+//
+// 4
+//
+// 1 2 3 2 3
+//
+// answer: 2
--- roman-numeral-converter.js
+++ roman-numeral-converter.js
@@ -15,36 +15,69 @@ function convertToRoman(num) {
     },
     res = "";
 
-  // I, II, III, "IV", V, VI, VII, VIII, IX, and X.
-  // Add function that handles 4 and 9.
-  // if multiple of 5?
+  // I, II, III, "IV", V, VI, VII, VIII, "IX", and X.
 
-  // Ones place
-  if (arr[0]) {
-    for (let j = 0; j < arr[0]; j++) {
-    res += romanNum.one;
+  // 1. Add function that handles the fours and the nines in any
+  // numerical place.
+
+  // 2. Using modulo could help?
+
+  console.log(0 % 5);
+  console.log(1 % 5);
+  console.log(2 % 5);
+  console.log(3 % 5);
+  console.log(4 % 5);
+  console.log(5 % 5);
+  console.log(6 % 5);
+  console.log(7 % 5);
+  console.log(8 % 5);
+  console.log(9 % 5);
+  console.log(10 % 5);
+
+  function handler(array) {
+    // ones
+    for (let j = 0; j < array[0]; j++) {
+      if (array[0] < 5) {
+
+        // maybe this can be a universal function!
+        if (array[0] % 5 === 4) {
+          res = romanNum.one + romanNum.five;
+        } else if (array[0] % 5 === 0) {
+          res = romanNum.five;
+        }
+
+      } else if (array[0] > 5) {
+        res = romanNum.five;
+        res += romanNum.one;
+      }
+
+
     }
 
     romanArr[0] = res;
     res = "";
-  }
-  // Tenths place
-  if (arr[1]) {
-    for (let k = 0; k < arr[1]; k++) {
-      res += romanNum.ten;
-    }
 
-    romanArr[1] = res;
-    res = "";
   }
+
+  handler(arr);
+
+  // Tenths place
+  // if (arr[1]) {
+  //   for (let k = 0; k < arr[1]; k++) {
+  //     res += romanNum.ten;
+  //   }
+  //
+  //   romanArr[1] = res;
+  //   res = "";
+  // }
 
   // Hundreths place
 
   // Thousandsths place
 
-  console.log(arr);
+
   console.log(romanArr);
   // return res;
 }
 
-convertToRoman(36);
+convertToRoman(7);
