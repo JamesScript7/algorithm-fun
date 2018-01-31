8b51dd61ed2427495a1e79333e518fa5502cb5e7
James Kim <yameskim@gmail.com>
2018-01-30 23:38:45 -0500

    Update birthday-candles

--- binary-agents.js
+++ binary-agents.js
@@ -1,5 +1,9 @@
 'use script';
 
+// Return an English translated sentence of the passed binary string.
+
+const binary = '01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111';
+
 function binaryAgent(str) {
   const binArr  = str.split(" "),
         res     = [];
@@ -9,12 +13,13 @@ function binaryAgent(str) {
     res.push(String.fromCharCode(parseInt(binArr[i],2)));
   }
 
-  // This is how to turn a character into binary:
+  // TIP: This is how to turn a character into binary!
   // console.log("q".charCodeAt(0).toString(2));
 
 	decrypted = res.join('');
-  console.log(decrypted);
+
+	console.log(decrypted);
   return decrypted;
 }
 
-binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+binaryAgent(binary);
--- birthday-candles.js
+++ birthday-candles.js
@@ -1,25 +1,37 @@
-var arr = [2,5,6,4,2,6,8,9,8,9,9,3];
-var n = arr.length;
-var maxNum = 0;
-var candleCount = 0;
-var maxArr = [];
+'use strict';
 
+/* Hackerrank Problem: Birthday Cake Candles.
+ *
+ * Basically the algorithm returns the total number
+ * of the largest integer in the array input.
+ *
+ * */
+
+// A simple and short sorting function:
 // var sortedArr = arr.sort((a,b) => a - b);
 // console.log(sortedArr);
 
-for (var i = 0; i < n; i++) {
-  if (arr[i] > maxNum) {
-    maxNum = arr[i];
-  }
-}
-maxArr.push(maxNum);
+const arr = [2,5,6,4,2,6,8,9,8,9,9,3];
 
-for (var j = 0; j < n; j++) {
-  if (arr[j] === maxNum) {
-    candleCount++;
+function largestCandleFinder(a) {
+	let n           = a.length,
+	    maxNum      = 0,
+	    maxArr      = [];
+
+  for (let i = 0; i < n; i++) {
+    if (arr[i] > maxNum) {
+      maxNum = arr[i];
+			maxArr = [];
+			maxArr.push(maxNum);
+    } else if (arr[i] === maxNum) {
+			maxArr.push(maxNum);
+		}
   }
+
+	const msg = `Largest Number is ${maxNum} and there are ${maxArr.length} of them.`;
+
+	console.log(msg);
+	return msg;
 }
 
-// They only wanted the total number of the tallest candle
-console.log(candleCount);
-console.log(maxArr);
+largestCandleFinder(arr);
