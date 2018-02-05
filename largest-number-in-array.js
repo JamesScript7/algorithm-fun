f4d119e6c29e9e52547c9cec4fdcd11af1202635
James Kim <yameskim@gmail.com>
2018-02-05 17:27:34 -0500

    Fix largest number in array

--- largest-number-in-array.js
+++ largest-number-in-array.js
@@ -12,20 +12,20 @@ const numArr = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 8
 
 function largestOfFour(arr) {
   let newArr = [];
-
-	for (let i = 0; i < arr.length; i++) {
-  	let maxNum = 0;
-
-  	for (let j = 0; j < arr[i].length; j++) {
-  		if ( arr[i][j] > maxNum ) {
-  			maxNum = arr[i][j];
-  		}
-		newArr[i] = maxNum;
-  	}
-  }
 	
-	console.log(newArr);
-	return newArr;
+  for (let i = 0; i < arr.length; i++) {
+    let maxNum = 0;
+
+    for (let j = 0; j < arr[i].length; j++) {
+  	  if ( arr[i][j] > maxNum ) {
+  		  maxNum = arr[i][j];
+  	  }
+    newArr[i] = maxNum;
+    }
+  }
+
+  console.log(newArr);
+  return newArr;
 }
 
 largestOfFour(numArr);
