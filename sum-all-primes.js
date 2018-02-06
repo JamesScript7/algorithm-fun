32b50c8447b3cf636b26348df9cc6567fab7198b
James Kim <yameskim@gmail.com>
2018-02-06 17:51:01 -0500

    Fix sum-all-primes

--- sum-all-primes.js
+++ sum-all-primes.js
@@ -4,11 +4,7 @@
  *
  * Sum all the prime numbers up to and including the provided number.
  *
- * One shortcut is to test the num up the square root instead of
- * iterating the entirety of num!
  * */
-
-'use strict';
 
 // Finds out if an integer is a prime number and returns a boolean value.
 function isPrime(num) {
